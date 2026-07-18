d89e890597d4cd04100cbdf67468405c9a35253b
Joaquin <007605813@coyote.csusb.edu>
2026-07-18 16:46:49 -0700

    added more questions

--- src/data/questions.js
+++ src/data/questions.js
@@ -2,21 +2,67 @@
 const questions = [
   {
     id: 1,
-    question: "What is the supreme law of the land?",
-    answer: "The Constitution",
+    question: "What is the form of government in the United States?",
+    answer: "Republic",
   },
   {
     id: 2,
-    question: "What does the Constitution do?",
-    answer:
-      "Sets up the government, defines the government, protects basic rights of Americans",
+    question: "What is the supreme law of the land?",
+    answer: "(U.S.) Constitution",
   },
   {
     id: 3,
-    question:
-      "The idea of self-government is in the first three words of the Constitution. What are these words?",
-    answer: "We the People",
+    question: "Name one thing the U.S. Constitution does.",
+    answer: "Protects the rights of the people",
   },
+  {
+    id: 4,
+    question: "The U.S. Constitution starts with the words \"We the People.\" What does \"We the People\" mean?",
+    answer: "Self-government",  
+  },
+  {
+    id: 5,
+    question: "How are changes made to the Constitution?",
+    answer: "Amendments",
+  },
+  {
+    id: 6,
+    question: "What does the Bill of Rights protect?",
+    answer: "(The basic) rights of Americans",
+  },
+  {
+    id: 7,
+    question: "How many amendments does the U.S.Constitution have?",
+    answer: "Twenty-seven (27)",
+  },
+  {
+    id: 8,
+    question: "Why is the Declaration of Independence important?",
+    answer: "It says America is free from British control",
+  },
+  {
+    id: 9,
+    question: "What founding document said the American colonies were free from Britain?",
+    answer: "Declaration of Independence",
+  },
+  {
+    id: 10,
+    question: "Name two important ideas from the Declaration of Independence and the U.S. Constitution.",
+    answer: "Equality and liberty",
+  },
+  {
+    id: 11,
+    question: "The words \"Life, Liberty, and the pursuit of Happiness\" are in what founding document?",
+    answer: "Declaration of Independence",
+  },
+  {
+    id: 12,
+    question: "What is the economic system in the United States?",
+    answer: "Capitalism", 
+  },
+  {
+    id: 13,
+  }
   // fill with the rest of the 128 questions
 ];
 
