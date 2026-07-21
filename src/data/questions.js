88899a8c1e871f4e873dbdb40d6867cf644c622f
Joaquinrdlt <joaquin.jr3@gmail.com>
2026-07-20 17:54:31 -0700

    added questions up to 60

--- src/data/questions.js
+++ src/data/questions.js
@@ -17,8 +17,9 @@ const questions = [
   },
   {
     id: 4,
-    question: "The U.S. Constitution starts with the words \"We the People.\" What does \"We the People\" mean?",
-    answer: "Self-government",  
+    question:
+      'The U.S. Constitution starts with the words "We the People." What does "We the People" mean?',
+    answer: "Self-government",
   },
   {
     id: 5,
@@ -42,28 +43,281 @@ const questions = [
   },
   {
     id: 9,
-    question: "What founding document said the American colonies were free from Britain?",
+    question:
+      "What founding document said the American colonies were free from Britain?",
     answer: "Declaration of Independence",
   },
   {
     id: 10,
-    question: "Name two important ideas from the Declaration of Independence and the U.S. Constitution.",
+    question:
+      "Name two important ideas from the Declaration of Independence and the U.S. Constitution.",
     answer: "Equality and liberty",
   },
   {
     id: 11,
-    question: "The words \"Life, Liberty, and the pursuit of Happiness\" are in what founding document?",
+    question:
+      'The words "Life, Liberty, and the pursuit of Happiness" are in what founding document?',
     answer: "Declaration of Independence",
   },
   {
     id: 12,
     question: "What is the economic system in the United States?",
-    answer: "Capitalism", 
+    answer: "Capitalism",
   },
   {
     id: 13,
-  }
-  // fill with the rest of the 128 questions
+    question: "What is the rule of law?",
+    answer: "Everyone must follow the law.",
+  },
+  {
+    id: 14,
+    question: "Many documents influenced the U.S. Constitution. Name one.",
+    answer: "Declaration of Independence",
+  },
+  {
+    id: 15,
+    question: "There are three branches of government. Why?",
+    answer: "So one part does not become too powerful",
+  },
+  {
+    id: 16,
+    question: "Name the three branches of government.",
+    answer: "Legislative, executive, and judicial",
+  },
+  {
+    id: 17,
+    question:
+      "The President of the United States is in charge of which branch of government?",
+    answer: "Executive branch",
+  },
+  {
+    id: 18,
+    question: "What part of the federal government writes laws?",
+    answer: "(U.S.) Congress",
+  },
+  {
+    id: 19,
+    question: "What are the two parts of the U.S. Congress?",
+    answer: "Senate and House (of Representatives)",
+  },
+  {
+    id: 20,
+    question: "Name one power of the U.S. Congress.",
+    answer: "Writes laws",
+  },
+  {
+    id: 21,
+    question: "How many senators are there?",
+    answer: "One hundred (100)",
+  },
+  {
+    id: 22,
+    question: "How long is a term for a U.S. Senator?",
+    answer: "Six (6) years",
+  },
+  {
+    id: 23,
+    question: "Who is one of your state's U.S. senators now?",
+    answer: "Alex Padilla",
+  },
+  {
+    id: 24,
+    question: "How many members are in the House of Representatives?",
+    answer: "Four hundred thirty-five (435)",
+  },
+  {
+    id: 25,
+    question:
+      "How long is a term for a member of the House of Representatives?",
+    answer: "Two (2) years ",
+  },
+  {
+    id: 26,
+    question:
+      "Why do U.S. representatives serve shorter terms than U.S. senators?",
+    answer: "To more closely follow public opinion",
+  },
+  {
+    id: 27,
+    question: "How many senators does each state have?",
+    answer: "Two (2)",
+  },
+  {
+    id: 28,
+    question: "Why does each state have two senators?",
+    answer: "Equal representation (for small states)",
+  },
+  {
+    id: 29,
+    question: "Name your U.S. representative.",
+    answer: "Raul Ruiz",
+  },
+  {
+    id: 30,
+    question:
+      "What is the name of the Speaker of the House of Representatives now?",
+    answer: "Mike Johnson",
+  },
+  {
+    id: 31,
+    question: "Who does a U.S. senator represent?",
+    answer: "People of their state",
+  },
+  {
+    id: 32,
+    question: "Who elects U.S. senators?",
+    answer: "Citizens from their state",
+  },
+  {
+    id: 33,
+    question: "Who does a member of the House of Representatives represent?",
+    answer: "Citizens in their district",
+  },
+  {
+    id: 34,
+    question: "Who elects members of the House of Representatives?",
+    answer: "Citizens from their (congressional) district",
+  },
+  {
+    id: 35,
+    question: "Some states have more representatives than other states. Why?",
+    answer: "(Because of) the state's population",
+  },
+  {
+    id: 36,
+    question:
+      "The President of the United States is elected for how many years?",
+    answer: "Four (4) years",
+  },
+  {
+    id: 37,
+    question:
+      "The president of the United States can serve only two terms. Why?",
+    answer: "(Because of) the 22nd Amendment",
+  },
+  {
+    id: 38,
+    question: "What is the name of the President of the United States now?",
+    answer: "Donald Trump",
+  },
+  {
+    id: 39,
+    question:
+      "What is the name of the Vice President of the United States now?",
+    answer: "JD Vance",
+  },
+  {
+    id: 40,
+    question: "If the president can no longer serve, who becomes president?",
+    answer: "The Vice President (of the United States)",
+  },
+  {
+    id: 41,
+    question: "Name one power of the president.",
+    answer: "Signs bills into law",
+  },
+  {
+    id: 42,
+    question: "Who is Commander in Chief of the U.S. military?",
+    answer: "The President (of the United States)",
+  },
+  {
+    id: 43,
+    question: "Who signs bills to become laws?",
+    answer: "The President (of the United States)",
+  },
+  {
+    id: 44,
+    question: "Who vetoes bills?",
+    answer: "The President (of the United States)",
+  },
+  {
+    id: 45,
+    question: "Who appoints federal judges?",
+    answer: "The President (of the United States)",
+  },
+  {
+    id: 46,
+    question: "The executive branch has many parts. Name one.",
+    answer: "President (of the United States)",
+  },
+  {
+    id: 47,
+    question: "What does the President's Cabinet do?",
+    answer: "Advice the President (of the United States)",
+  },
+  {
+    id: 48,
+    question: "What are two Cabinet-level positions?",
+    answer: "Secretary of Energy and Secretary of Education",
+  },
+  {
+    id: 49,
+    question: "Why is the Electoral College important?",
+    answer: "It decides who is elected president ",
+  },
+  {
+    id: 50,
+    question: "What is one part of the judicial branch?",
+    answer: "Supreme Court",
+  },
+  {
+    id: 51,
+    question: "What does the judicial branch do?",
+    answer: "Reviews laws",
+  },
+  {
+    id: 52,
+    question: "What is the highest court in the United States?",
+    answer: "Supreme Court",
+  },
+  {
+    id: 53,
+    question: "How many seats are on the Supreme Court?",
+    answer: "Nine (9)",
+  },
+  {
+    id: 54,
+    question:
+      "How many Supreme Court justices are usually needed to decided a case?",
+    answer: "Five (5)",
+  },
+  {
+    id: 55,
+    question: "How long do Supreme Court justices serve?",
+    answer: "(For) life",
+  },
+  {
+    id: 56,
+    question: "Supreme Court justices serve for life. Why?",
+    answer: "To be independent (of politics)",
+  },
+  {
+    id: 57,
+    question: "Who is the Chief Justice of the United States now?",
+    answer: "John Roberts",
+  },
+  {
+    id: 58,
+    question: "Name one power that is only for the federal government.",
+    answer: "Print paper money ",
+  },
+  {
+    id: 59,
+    question: "Name one power that is only for the states.",
+    answer: "Provide schooling and education",
+  },
+  {
+    id: 60,
+    question: "What is the purpose of the 10th Amendment?",
+    answer:
+      "(It states that the) powers not given to the federal government belong to the states or to the people.",
+  },
+  {
+    id: 60,
+    question: "",
+    answer: "",
+  },
 ];
 
 export default questions;
